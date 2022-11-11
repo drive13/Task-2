4cd8cb372cea79e01cd2b0e57371d4261bfed335
drive13 <mymaindrive13@gmail.com>
2022-11-11 21:18:48 +0700

    create login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
@@ -54,4 +54,88 @@
             </div>
         </form>
     </x-auth-card>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ "Task-2" }} - {{ "Login" }}</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    
+    <link rel="shortcut icon" href="assets/images/favicon.svg" type="image/x-icon">
+    <link rel="stylesheet" href="assets/css/app.css">
+</head>
+
+<body>
+    <div id="auth">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5 col-sm-12 mx-auto">
+                    <div class="card pt-4">
+                        <div class="card-body">
+                            <div class="text-center mb-5">
+                                <img src="assets/images/favicon.svg" height="48" class='mb-4'>
+                                <h3>Login</h3>
+                                <p>Please log in to continue.</p>
+                            </div>
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+                                <div class="form-group position-relative has-icon-left">
+                                    <label for="email">Email</label>
+                                    <div class="position-relative">
+                                        <input type="text" class="form-control @error('email') is_invalid @enderror" id="email" name="email" value="{{ old('email') }}" required autofocus>
+                                        <div class="form-control-icon">
+                                            <i data-feather="user"></i>
+                                        </div>
+                                    </div>
+                                    @error('email')
+                                        <small class="text-red">{{ $message }}</small>
+                                    @enderror
+                                </div>
+                                <div class="form-group position-relative has-icon-left">
+                                    <div class="clearfix">
+                                        <label for="password">Password</label>
+                                        <a href="{{ route('password.request') }}" class='float-end'>
+                                            <small>Forgot password?</small>
+                                        </a>
+                                    </div>
+                                    <div class="position-relative">
+                                        <input type="password" class="form-control @error('password') is_invalid @enderror" id="password" name="password" value="{{ old('password') }}" required>
+                                        <div class="form-control-icon">
+                                            <i data-feather="lock"></i>
+                                        </div>
+                                    </div>
+                                    @error('password')
+                                        <small class="text-red">{{ $message }}</small>
+                                    @enderror
+                                </div>
+
+                                <div class='form-check clearfix my-4'>
+                                    <div class="checkbox float-start">
+                                        <input type="checkbox" id="checkbox1" class='form-check-input' name="remember">
+                                        <label for="checkbox1">Remember me</label>
+                                    </div>
+                                    <div class="float-end">
+                                        Don't have an account?<a href="{{ route('register') }}"> Register</a>
+                                    </div>
+                                </div>
+                                <div class="row clearfix">
+                                    <button class="btn btn-primary float-end">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="assets/js/feather-icons/feather.min.js"></script>
+    <script src="assets/js/app.js"></script>
+    
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
